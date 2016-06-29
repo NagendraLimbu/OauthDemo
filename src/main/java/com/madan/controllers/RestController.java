1fe47ee828d17223897cf3bfe23b17ab3d8449a9
Madan <madan.siwakoti809@gmail.com>
2016-06-28 20:28:30 -0500

    Signed-off-by: Madan <madan.siwakoti809@gmail.com>

---
+++ src/main/java/com/madan/controllers/RestController.java
@@ -0,0 +1,31 @@
+package com.madan.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.madan.models.User;
+import com.madan.services.DataService;
+
+/**
+ * @author Madan
+ *
+ */
+@Controller
+@RequestMapping("/check/users")
+public class RestController {
+
+	@Autowired
+	DataService dataService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public List<User> list() {
+		return dataService.getUserList();
+
+	}
+}
---
+++ src/main/java/com/madan/services/DataService.java
@@ -0,0 +1,12 @@
+package com.madan.services;
+
+import java.util.List;
+
+import com.madan.models.User;
+/**
+ * @author Madan
+ *
+ */
+public interface DataService {
+	public List<User> getUserList();
+}
